9bff7f4516b203c98fb782f7e46ea46bcf9933da
Deokhaeng Lee <jefflee.ldh@gmail.com>
2020-10-11 00:54:47 +0900

    socketio in process..

--- socketio/app.py
+++ socketio/app.py
@@ -12,10 +12,24 @@ def main():
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
-    
+
 if __name__ == '__main__':
-    socketio.run(app, host = "0.0.0.0", port=5000)
+    socketio.run(app, host = "127.0.0.1", port=5000)
